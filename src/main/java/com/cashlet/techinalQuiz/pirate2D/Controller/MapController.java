226c947f20bb886e178b0bdc0168cc7c4f390244
Onyetube Kosisochukwu <kosisochukwu@cbitindustries.com>
2021-04-15 01:26:28 +0000

    Made some modification on the algorithm

--- src/main/java/com/cashlet/techinalQuiz/pirate2D/Controller/MapController.java
+++ src/main/java/com/cashlet/techinalQuiz/pirate2D/Controller/MapController.java
@@ -1,8 +1,6 @@
 package com.cashlet.techinalQuiz.pirate2D.Controller;
 
-import com.cashlet.techinalQuiz.pirate2D.Model.Coordinate;
-import com.cashlet.techinalQuiz.pirate2D.Model.Request;
-import com.cashlet.techinalQuiz.pirate2D.Utils.Game2DPath;
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -10,8 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RestController()
@@ -20,38 +17,71 @@ public class MapController {
     Object obj = new Object();
     @PostMapping(value = "/map", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
-    ResponseEntity<String> MapItem(@RequestBody Object request[][]){
-        logger.info(String.valueOf(request.length));
+    ResponseEntity<Map<Object, Object>> MapItem(@RequestBody Object request[][]){
         obj = request;
-        return ResponseEntity.accepted().body("");
+        Map<Object, Object> response = new HashMap<>();
+        response.put("message", "Map added successfully");
+        response.put("success", true);
+        return ResponseEntity.accepted().body(response);
     }
 
     @GetMapping(value = "/findPath", produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody Object GetItem(@RequestParam("startXPosition") int startXPosition,
+    public @ResponseBody ResponseEntity<Object> GetItem(@RequestParam("startXPosition") int startXPosition,
                                         @RequestParam("startYPosition") int startYPosition,
                                         @RequestParam("targetXPosition") int targetXPosition,
                                         @RequestParam("targetYPosition") int targetYPosition){
 
-        Object data[][] = (Object[][]) obj;
-        Coordinate startCoordinate = new Coordinate(startXPosition,startYPosition);
-        Coordinate endCoordinate = new Coordinate(targetXPosition,targetYPosition);
-        Object response[] = {};
-//        for (int a = 0; a <= data.length; a ++) {
-//            for (int i = targetXPosition; i <= a; i++) {
-//                for (int j = a; j <= targetYPosition; j++) {
-//                    System.out.println("======================");
-//                    System.out.println("Index J: " + j);
-//                    System.out.println("Index I: " + i);
-//                    System.out.println(data[i][j]);
-////                    respone[]
-//                    System.out.println("======================");
-//                }
-//            }
-//        }
-//        System.out.println(data.get(0).get(0));
-//        System.out.println(data.get(1).get(0));
-        Game2DPath game2DPath = new Game2DPath();
-        game2DPath.Init(data, startCoordinate, endCoordinate);
-        return data;
+        Map<String, Object> response = new HashMap<>();
+        try {
+            //Assign save map and cast to 2d array object
+            Object data[][] = (Object[][]) obj;
+            Set<ArrayList<Integer>> paths = new HashSet<>();
+            Set<Object> items = new HashSet<>();
+            int TotalAmount = 0;
+            //Let loop through the columns
+            for (int i = 0; i < data.length; i++) {
+                //let loop through the rows
+                for (int j = 0; j < data[i].length; j++) {
+                    //let check if the position is lesser than or equals to the target positions
+                    if (targetXPosition <= i && targetYPosition >= i) {
+                        ArrayList<Integer> patha = new ArrayList<>();
+                        logger.info(data[i][j] + " ");
+                        logger.info("point [{}, {}]", targetXPosition - i, targetYPosition - j);
+                        patha.add(i);
+                        patha.add(j);
+                        paths.add(patha);
+                        //let store item in a set list to prevent duplication
+                        items.add(data[i][j]);
+                    }
+                    if (targetXPosition <= j && targetYPosition >= j) {
+                        ArrayList<Integer> addPath = new ArrayList<>();
+                        logger.info(data[i][j] + " ");
+                        logger.info("point [{}, {}]", targetXPosition - i, targetYPosition - j);
+                        addPath.add(i);
+                        addPath.add(j);
+                        paths.add(addPath);
+                        //let store item in a set list to prevent duplication
+                        items.add(data[i][j]);
+                    }
+                }
+            }
+            //Let calculate the total coins
+            for (Object obj : items) {
+                Map<Map<Object, Object>, Map<Object, Object>> newArry2 = (Map<Map<Object, Object>, Map<Object, Object>>) obj;
+                if (newArry2.containsValue("coin")) {
+                    TotalAmount += Integer.parseInt(String.valueOf(newArry2.get("amount")));
+                }
+            }
+            ;
+            logger.info("Total Amount: {}", TotalAmount);
+            logger.info("All Paths: {}", new Gson().toJson(paths));
+            logger.info("All Items: {}", new Gson().toJson(items));
+            response.put("paths", paths);
+            response.put("coins", TotalAmount);
+        }catch(Exception ex){
+            response.put("error", ex.getMessage());
+            return ResponseEntity.status(500).body(response);
+        }
+        return ResponseEntity.ok(response);
     }
 }
